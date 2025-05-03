0e5bbbb767cd1027b9c2eb3f183c0f8c491c82a9
Citicomp9835 <87692052+Citicomp9835@users.noreply.github.com>
2025-05-03 11:13:57 +0800

    Add files via upload

--- exc02.cpp
+++ exc02.cpp
@@ -24,14 +24,27 @@ int main()
 
   // std::shared_ptr<Cint> cint;
   // std::shared_ptr<Cint> spCint(new Cint(8,9));
-  std::shared_ptr<Cint> spCint(new Cint);
+
+  std::shared_ptr<Cint> e = std::shared_ptr<Cint>(new Cint(8,9));
+  std::shared_ptr<Cint> f = std::shared_ptr<Cint>(new Cint(40,30));
+
   std::shared_ptr<Cint> spCinta;
+  std::shared_ptr<Cint> spCint(new Cint);
+
   spCint->x = 10 ;
   spCinta = spCint;
   spCint->x = 20 ;
 
+  std::vector<std::shared_ptr<Cint>> spVec;
+  spVec.push_back(spCint);
+  spVec.push_back(spCinta);
+  spVec.push_back(e);
+  spVec.push_back(f);
+
+
   std::cout << "Cint Vaule : " << cintb.x << " " << cintb.y << "\n";
   std::cout << "Vector Vaule : " << cintVec[0].x << " " << cintVec[0].y << "\n";
   std::cout << "Shared Pointer Vaule : " << spCinta->x << "\n";
+  std::cout << "Vector Shared Pointer Value : " << spVec[3]->x << "\n";
   return 0;
 }
