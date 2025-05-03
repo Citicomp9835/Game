27aa789eaa145a68cd7463a4661656f43b3655e6
Citicomp9835 <87692052+Citicomp9835@users.noreply.github.com>
2025-05-03 12:56:16 +0800

    Add files via upload

--- exc02.cpp
+++ exc02.cpp
@@ -14,17 +14,23 @@ public:
 
 int main()
 {
+  //Simple class with int x and int y
   Cint cinta(2,3);
   Cint cintb;
   cintb.x=4;
   cintb.y=5;
 
+  // Create Vector for Class Cint
   std::vector<Cint> cintVec;
+  
+  // Insert class to Vector
   cintVec.push_back(cinta);
 
   // std::shared_ptr<Cint> cint;
   // std::shared_ptr<Cint> spCint(new Cint(8,9));
 
+  // Create Shared Pointer for Class Cint
+  
   std::shared_ptr<Cint> e = std::shared_ptr<Cint>(new Cint(8,9));
   auto f = std::shared_ptr<Cint>(new Cint(40,30));
 
@@ -35,7 +41,10 @@ int main()
   spCinta = spCint;
   spCint->x = 20 ;
 
+  // Create Vector for Shared Pointer
   std::vector<std::shared_ptr<Cint>> spVec;
+  
+  // Insert Shared Pointer to Vector
   spVec.push_back(spCint);
   spVec.push_back(spCinta);
   spVec.push_back(e);
